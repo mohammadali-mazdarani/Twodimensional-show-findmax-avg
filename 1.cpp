b6a33a90e0c6ec700b141444b775d73a8dd6e4e5
Mohammad ali mazdarani <mazdarani26.gmail.com>
2022-12-23 05:34:45 -0800

    calc avg and change to find max

--- 1.cpp
+++ 1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <conio.h>
 #include <math.h>
+#include <iomanip>
 using namespace std;
 /* run this program using the console pauser or add your own getch, system(
 ) or input loop */
@@ -14,6 +15,10 @@ using namespace std;
 void show(int b[][2],int row,int col);
 
 int max(int b[3][2],int row,int col);
+
+float avg(int b[][2],int rows,int col);
+
+void findmaxind(int b[][2],int rows,int col,int max);
 //--------------------------
 //micro
 //---------------------
@@ -22,7 +27,7 @@ int max(int b[3][2],int row,int col);
 int main(int argc, char** argv)
 {
 //Declar array and show variable
-int a[3][2] = {{15,18} , {19,12} , {14,17}};
+int a[3][2] = {{15,18} , {19,12} , {14,117}};
 
 //Show array
 show(a,3,2);
@@ -32,7 +37,25 @@ show(a,3,2);
 cout<<endl<<"-------------------------"<<endl;
 
 //Maximum of values
-cout<<"Maximum of values = "<<max(a,3,2);
+
+int max2 = max(a,3,2);
+
+cout<<"Maximum of values = "<<max2;
+
+//Seperator
+cout<<endl<<"-------------------------"<<endl;
+
+//Show of index array
+
+findmaxind(a,3,2,max2);
+
+
+//Seperator
+cout<<endl<<"-------------------------"<<endl;
+
+//avarage of number
+    cout<<fixed<<setprecision(2);
+cout<<"Average of number = "<<avg(a,3,2);
 
 getch();
 }
@@ -80,4 +103,51 @@ int max(int b[3][2],int row,int col)
      return max;
 }
 
+//--------------------------------------------
+
+//This function find index if max
+void findmaxind(int b[][2],int rows,int col,int max)
+{
+
+
+     for(int i = 0; i<rows; ++i)
+     {
+        for(int j = 0; j<col; ++j)
+        {
+         
+            if(max == b[i][j])
+            {
+
+            cout<<"Index of max array = "<<"b["<<i<<"]"<<"["<<j<<"]";
+            cout<<endl;
+            cout<<"Max in Rows"<<(i+1);
+            cout<<endl;
+
+            }
+        }
+     }
+
+}
+
 //---------------------------------------------
+
+//Function for Calc avarage
+
+float avg(int b[][2],int rows,int col)
+{
+
+float avg1;
+float sum = 0;
+
+for(int i=0; i<rows; ++i)
+{
+    for(int j=0; j<col; ++j)
+    {
+        sum += b[i][j];
+    }
+}
+
+avg1 = sum/(rows * col);
+
+return avg1;
+}
