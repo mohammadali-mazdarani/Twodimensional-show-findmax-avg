0485fbee93fd246142b186b816122c75c2fdf5fe
Mohammad ali mazdarani <mazdarani26.gmail.com>
2022-12-23 00:15:16 -0800

    initial commit

---
+++ 1.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <conio.h>
+#include <math.h>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system(
+) or input loop */
+//create namespace
+/*Branch name
+*
+*
+*/
+//prototype
+
+//--------------------------
+//micro
+//---------------------
+//global variable
+//-------------------
+int main(int argc, char** argv)
+{
+
+int a[3][2] = {{15,18} , {19,12} , {14,17}};
+
+
+getch();
+}
+//function variable
+
+//--------------------------
+//functions
+
+//for show variable of function
+void show(int b[][2],int row,int col)
+{
+    for (int i=0; i<row; ++i)
+    {
+        cout<<"Row "<<(i + 1)<<" : "
+        for(int j=0; j<col; ++j)
+        {
+            
+        }
+    }
+}
+
